2fe27421882f8dd0aa20bb6c6a326c676b343d4b
MARCO ANTONIO MERCADO <19121052@morelia.tecnm.mx>
2021-07-10 18:05:37 -0500

    Agregando diseño y activity para calificaciones...

---
+++ app/src/main/java/mx/edu/itm/mark/tap_u4_proyecto/CalificacionesActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.itm.mark.tap_u4_proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalificacionesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calificaciones)
+    }
+}
